569296e8f5397f413671ec9d626215e08b07d200
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2016-03-26 21:37:19 +0100

    :bear: finish Chapter 12

--- 12 - Displaying State with JSX/scripts/main.js
+++ 12 - Displaying State with JSX/scripts/main.js
@@ -60,8 +60,16 @@ var App = React.createClass({
 
 var Fish = React.createClass({
   render: function () {
+    var details = this.props.details;
     return (
-      <li>{this.props.index}</li>
+      <li className="menu-fish">
+        <img src={details.image} alt={details.name}/>
+        <h3 className="fish-name">
+          {details.name}
+          <span className="price">{h.formatPrice(details.price)}</span>
+        </h3>
+        <p>{details.desc}</p>
+      </li>
     );
   }
 });
