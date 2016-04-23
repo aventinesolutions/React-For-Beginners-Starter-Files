b04399a3828e7f253dadb6da92db08bc4cd0d479
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2016-04-23 21:37:37 +0200

    :bear: added Github OAuth authentication/authorization
    :bear: added Login in with Github OAuth
    
    :bear: save the Github token
    
    ... in local storage so we have some kind of persistent session
    
    :bear: logout action
    .
    
    :bear: put Firebase Endpoint in a shred config
    .

---
+++ 29 - Firebase Authentication/scripts/components/Inventory.js
@@ -0,0 +1,143 @@
+/*
+  Inventory
+  <Inventory/>
+*/
+
+import config from '../config';
+
+import React from 'react';
+import AddFishForm from './AddFishForm';
+import autobind from 'autobind-decorator';
+
+import Firebase from 'firebase';
+const ref = new Firebase(config.firebaseEndpoint);
+
+@autobind
+class Inventory extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      uid: ''
+    };
+  }
+
+  authenticate(provider) {
+    ref.authWithOAuthPopup(provider, this.authHandler);
+  }
+
+  componentWillMount() {
+    let token = localStorage.getItem('token');
+    if(token) {
+      ref.authWithCustomToken(token, this.authHandler);
+    }
+  }
+
+  authHandler(err, authData) {
+    if(err) {
+      console.err(err);
+      return;
+    }
+
+    // save the login token in the browser
+    localStorage.setItem('token', authData.token);
+
+    const storeRef = ref.child(this.props.params.storeId);
+    storeRef.on('value', (snapshot) => {
+      let data = snapshot.val() || {};
+
+      if (!data.owner) {
+        // claim this store as our own if there is no owner already
+        storeRef.set({
+          owner: authData.uid
+        });
+      }
+
+      // update our state to reflect the current store owner and user
+      this.setState({
+        uid: authData.uid,
+        owner: (data.owner || authData.uid)
+      });
+    });
+  }
+
+  logout() {
+    ref.unauth();
+    localStorage.removeItem('token');
+    this.setState({
+      uid: null
+    });
+  }
+
+  renderLogin() {
+    return(
+      <nav className="login">
+        <h2>Inventory</h2>
+        <p>Sign in to manage your store's inventory</p>
+        <button className="github" onClick={this.authenticate.bind(this, 'github')}>Login with Github</button>
+      </nav>
+    );
+  }
+  
+  renderInventory(key) {
+    var linkState = this.props.linkState;
+    return (
+      <div className="fish-edit" key={key}>
+        <input type="text" valueLink={linkState('fishes.'+ key +'.name')}/>
+        <input type="text" valueLink={linkState('fishes.'+ key +'.price')}/>
+        <select valueLink={linkState('fishes.' + key + '.status')}>
+          <option value="unavailable">Sold Out!</option>
+          <option value="available">Fresh!</option>
+        </select>
+
+        <textarea valueLink={linkState('fishes.' + key + '.desc')}></textarea>
+        <input type="text" valueLink={linkState('fishes.'+ key +'.image')}/>
+        <button onClick={this.props.removeFish.bind(null, key)}>Remove Fish</button>
+        
+      </div>
+    )
+  }
+
+  render() {
+    let logoutButton = <button onClick={this.logout}>Log Out!</button>
+
+    // first check if they aren't logged in
+    if(!this.state.uid) {
+      return (
+        <div>{this.renderLogin()}</div>
+      );
+    }
+
+    // then check if they aren't the owner of the current store
+    if(this.state.uid !== this.state.owner) {
+      return (
+        <div>
+          <p>Sorry, you aren't the owner of this store</p>
+          {logoutButton}
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <h2>Inventory</h2>
+        {logoutButton}
+        
+        {Object.keys(this.props.fishes).map(this.renderInventory)}
+
+        <AddFishForm {...this.props} />
+        <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
+      </div>
+    )
+  }
+};
+
+Inventory.propTypes = {
+    addFish : React.PropTypes.func.isRequired,
+    loadSamples : React.PropTypes.func.isRequired,
+    fishes : React.PropTypes.object.isRequired,
+    linkState : React.PropTypes.func.isRequired,
+    removeFish : React.PropTypes.func.isRequired
+}
+
+export default Inventory;
